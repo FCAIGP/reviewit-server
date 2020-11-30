f4fe16887301b1dcb977a989244e003a958f5071
Ahmed Dardery <megadardery@yahoo.com>
2020-11-30 18:32:20 +0200

    feat: StatusChangeRequest

---
+++ Company_Reviewing_System/Models/CloseOptions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Company_Reviewing_System.Models
+{
+    public enum CloseOptions
+    {
+        Open,
+        ReOpened,
+        ViewOnly,
+        Erased,
+    }
+}
---
+++ Company_Reviewing_System/Models/StatusChangeRequest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Company_Reviewing_System.Models
+{
+    public class StatusChangeRequest
+    {
+        public string RequestId { get; set; }
+        public CloseOptions NewStatus { get; set; }
+        public string Description { get; set; }
+        public string Title { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
